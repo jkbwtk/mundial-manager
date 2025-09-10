54763fd130ca4d7db0d7bf641ba2860a35b03a7a
Jakub Wiątek <92359584+jkbwtk@users.noreply.github.com>
2025-09-10 23:32:18 +0200

    Add WIP message to Homepage

--- frontend/pages/Homepage/Homepage.tsx
+++ frontend/pages/Homepage/Homepage.tsx
@@ -1,5 +1,14 @@
+import { A } from '@solidjs/router';
+
 const Homepage: Component = () => {
-  return <div />;
+  return (
+    <>
+      <div>Still in development</div>
+      <div>
+        Test pages are available at <A href="/tests">here</A>
+      </div>
+    </>
+  );
 };
 
 export default Homepage;
