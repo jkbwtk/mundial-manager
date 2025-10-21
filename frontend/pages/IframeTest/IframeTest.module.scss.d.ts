e5344aa29ed9089dec2854a617d67106df3e8be1
Jakub Wiątek <92359584+jkbwtk@users.noreply.github.com>
2025-10-21 18:51:35 +0200

    Add iframe testing utilities

--- frontend/pages/IframeTest/IframeTest.module.scss.d.ts
+++ frontend/pages/IframeTest/IframeTest.module.scss.d.ts
@@ -1,3 +1,4 @@
 export declare const container: string;
 export declare const controls: string;
 export declare const iframe: string;
+export declare const iframeContainer: string;
--- frontend/pages/IframeTest/IframeTest.tsx
+++ frontend/pages/IframeTest/IframeTest.tsx
@@ -1,22 +1,90 @@
-import { createSignal } from 'solid-js';
+import { createSignal, onCleanup, onMount } from 'solid-js';
+import { isServer } from 'solid-js/web';
+import { Break } from '#components/Break';
+import { Button } from '#components/Button';
 import { Divider, Widget } from '#components/Widget';
+import { useSheets } from '#providers/SheetsProvider';
 import style from './IframeTest.module.scss';
 
+const AVAILABLE_URLS = [
+  import.meta.env.VITE_CALCULATOR_URL,
+  'http://localhost:4200/tests/iframe-test',
+  'http://127.0.0.1/tests/iframe-test',
+];
+
 const IframeTest: Component = () => {
-  const [url] = createSignal(import.meta.env.VITE_CALCULATOR_URL);
+  const [sheets, { latestMatchStats }] = useSheets();
+
+  // biome-ignore lint/style/useConst: yeah
+  let ref: HTMLIFrameElement = null!;
+  const [index, setIndex] = createSignal(0);
+
+  const url = () => AVAILABLE_URLS[index()];
+
+  const handleMessage = (ev: MessageEvent) => {
+    console.log(ev);
+  };
+
+  const sendMessage = () => {
+    ref.contentWindow?.postMessage({ info: 'Test message' }, '*');
+  };
+
+  const sendPlayers = () => {
+    const players = latestMatchStats().generalStats.uniquePlayers;
+    ref.contentWindow?.postMessage({ players }, '*');
+  };
+
+  const sendToParent = () => {
+    window.parent.postMessage({ info: 'iframe test message' }, '*');
+  };
+
+  onMount(() => {
+    if (isServer === false) {
+      window.addEventListener('message', handleMessage);
+    }
+  });
+
+  onCleanup(() => {
+    if (isServer === false) {
+      window.removeEventListener('message', handleMessage);
+    }
+  });
 
   return (
     <Widget topLeftLabels="Iframe Test Page" class={style.container}>
       <div class={style.controls}>
+        <Button
+          severity="secondary"
+          onClick={() => setIndex((i) => (i + 1) % AVAILABLE_URLS.length)}
+        >
+          Next URL
+        </Button>{' '}
         <strong>URL:</strong> <code>{url()}</code>
+        <Break />
+        <Button severity="secondary" onClick={sendMessage}>
+          Send Message
+        </Button>
+        <Button
+          severity="secondary"
+          disabled={sheets.ready === false}
+          onClick={sendPlayers}
+        >
+          Send Players
+        </Button>
+        <Button severity="secondary" onClick={sendToParent}>
+          Send to Parent
+        </Button>
       </div>
       <Divider />
-      <iframe
-        src={url()}
-        title="Test Iframe"
-        class={style.iframe}
-        sandbox="allow-scripts allow-same-origin allow-forms"
-      />
+      <div class={style.iframeContainer}>
+        <iframe
+          ref={ref}
+          src={url()}
+          title="Test Iframe"
+          class={style.iframe}
+          sandbox="allow-scripts allow-same-origin allow-forms"
+        />
+      </div>
     </Widget>
   );
 };
