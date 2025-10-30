a13a4570180b65552398e0d7feafc95ed1459832
Jakub Wiątek <92359584+jkbwtk@users.noreply.github.com>
2025-10-30 20:57:14 +0100

    Update test modal

--- frontend/pages/ModalTest/ModalTest.tsx
+++ frontend/pages/ModalTest/ModalTest.tsx
@@ -1,7 +1,11 @@
 import { createSignal } from 'solid-js';
+import { Break } from '#components/Break';
 import { Button } from '#components/Button';
+import { InlineAction } from '#components/InlineAction';
+import { Input } from '#components/Input';
+import { MaterialSymbol } from '#components/MaterialSymbol';
 import { Modal } from '#components/Modal';
-import { Widget } from '#components/Widget';
+import { Divider, Widget } from '#components/Widget';
 import { useModal, useModalActions } from '#providers/ModalProvider';
 import style from './ModalTest.module.scss';
 
@@ -9,13 +13,39 @@ const M: Component<{
   counter: number;
 }> = (props) => {
   const [a, setA] = createSignal(0);
+  const [inputValue, setInputValue] = createSignal('');
   const { closeModal } = useModalActions();
 
   return (
     <Modal topLeftLabels="Test Modal">
-      Modal Content {a()}, counter {props.counter}
-      <Button onClick={() => setA((v) => v + 1)}>Increment</Button>
-      <Button onClick={() => closeModal('test')}>Close Modal</Button>
+      <div>Counter from parent: {props.counter}</div>
+      <div>Local state: {a()}</div>
+      <Divider />
+      <div>Input field:</div>
+      <Input
+        value={inputValue()}
+        onInput={(e) => setInputValue(e.currentTarget.value)}
+        placeholder="Type something..."
+      />
+      <div>Current value: {inputValue()}</div>
+      <Divider />
+      <div>Buttons:</div>
+      <Button onClick={() => setA((v) => v + 1)}>
+        <MaterialSymbol symbol="arrow_upward" /> Increment
+      </Button>
+      <Button severity="secondary" onClick={() => setA(0)}>
+        <MaterialSymbol symbol="swap_vert" /> Reset
+      </Button>
+      <Button severity="danger" onClick={() => closeModal('test')}>
+        <MaterialSymbol symbol="check_box" /> Close Modal
+      </Button>
+      <Divider />
+      <div>Inline action:</div>
+      <InlineAction
+        symbol="t"
+        content="Press 't' or click"
+        onAction={(type) => console.log('Action triggered:', type)}
+      />
     </Modal>
   );
 };
@@ -25,12 +55,16 @@ const ModalTest: Component = () => {
   const [counter, setCounter] = createSignal(0);
 
   const openModal = () => {
-    const m = open({ component: M, counter: counter() }, (v) => {
+    open({ component: M, counter: counter() }, (v) => {
       console.log(v);
     });
 
     setCounter((c) => c + 1);
   };
+
+  // onMount(() => {
+  //   open({ component: M, counter: counter() });
+  // });
 
   return (
     <Widget topLeftLabels="Modal Test Page" class={style.container}>
@@ -40,6 +74,8 @@ const ModalTest: Component = () => {
       <Button severity="danger" onClick={openModal}>
         Open Basic Modal
       </Button>
+      <Break />
+      {/* <M counter={0} /> */}
     </Widget>
   );
 };
