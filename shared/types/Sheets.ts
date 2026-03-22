5ca0fbb97a2f2916662cf4ba7b33524df05dde10
Jakub Wiątek <92359584+jkbwtk@users.noreply.github.com>
2026-03-22 18:35:46 +0100

    Declare match event types as object literal

--- shared/types/Sheets.ts
+++ shared/types/Sheets.ts
@@ -10,12 +10,17 @@ export interface SheetMetadata {
   columns: number;
 }
 
-export const MatchEventType = z.enum([
-  'GOAL',
-  'POSITION_CHANGE',
-  'BALL_OUT',
-  'EQUIPMENT_FAILURE',
-]);
+export const MatchEventTypeEnum = {
+  GOAL: 'GOAL',
+  POSITION_CHANGE: 'POSITION_CHANGE',
+  BALL_OUT: 'BALL_OUT',
+  EQUIPMENT_FAILURE: 'EQUIPMENT_FAILURE',
+  PAUSE: 'PAUSE',
+  RESUME: 'RESUME',
+  CANCEL: 'CANCEL',
+} as const;
+
+export const MatchEventType = z.enum(MatchEventTypeEnum);
 
 export type MatchEventType = z.infer<typeof MatchEventType>;
 
@@ -24,7 +29,7 @@ const MatchEventBase = z.object({
 });
 
 export const MatchEventGoal = MatchEventBase.extend({
-  type: z.literal('GOAL'),
+  type: z.literal(MatchEventTypeEnum.GOAL),
   for: z.string(), // team color
   by: z.string(), //team color
   player: z.string().nullable().catch(null),
@@ -33,20 +38,20 @@ export const MatchEventGoal = MatchEventBase.extend({
 export type MatchEventGoal = z.infer<typeof MatchEventGoal>;
 
 export const MatchEventPositionChange = MatchEventBase.extend({
-  type: z.literal('POSITION_CHANGE'),
+  type: z.literal(MatchEventTypeEnum.POSITION_CHANGE),
   side: z.string(), // team color
 });
 
 export type MatchEventPositionChange = z.infer<typeof MatchEventPositionChange>;
 
 export const MatchEventBallOut = MatchEventBase.extend({
-  type: z.literal('BALL_OUT'),
+  type: z.literal(MatchEventTypeEnum.BALL_OUT),
 });
 
 export type MatchEventBallOut = z.infer<typeof MatchEventBallOut>;
 
 export const MatchEventEquipmentFailure = MatchEventBase.extend({
-  type: z.literal('EQUIPMENT_FAILURE'),
+  type: z.literal(MatchEventTypeEnum.EQUIPMENT_FAILURE),
   details: z.string().nullable().catch(null),
 });
 
@@ -55,20 +60,20 @@ export type MatchEventEquipmentFailure = z.infer<
 >;
 
 export const MatchEventPause = MatchEventBase.extend({
-  type: z.literal('PAUSE'),
+  type: z.literal(MatchEventTypeEnum.PAUSE),
   reason: z.string().nullable().catch(null),
 });
 
 export type MatchEventPause = z.infer<typeof MatchEventPause>;
 
 export const MatchEventResume = MatchEventBase.extend({
-  type: z.literal('RESUME'),
+  type: z.literal(MatchEventTypeEnum.RESUME),
 });
 
 export type MatchEventResume = z.infer<typeof MatchEventResume>;
 
 export const MatchEventCancel = MatchEventBase.extend({
-  type: z.literal('CANCEL'),
+  type: z.literal(MatchEventTypeEnum.CANCEL),
   reason: z.string().nullable().catch(null),
 });
 
