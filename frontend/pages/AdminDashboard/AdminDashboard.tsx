2652550390daf7a5ac041ba1daddf95be40958a8
Jakub Wiątek <92359584+jkbwtk@users.noreply.github.com>
2026-05-11 23:13:35 +0200

    Use Table component in AdminDashboard

--- frontend/pages/AdminDashboard/AdminDashboard.tsx
+++ frontend/pages/AdminDashboard/AdminDashboard.tsx
@@ -1,6 +1,7 @@
 import { action, createAsync, json, query, useAction } from '@solidjs/router';
-import { For, Show } from 'solid-js';
+import { Show } from 'solid-js';
 import { Button } from '#components/Button';
+import { type Column, Table } from '#components/Table';
 import { Divider, Widget } from '#components/Widget';
 import { useHandleButtonAction } from '#flib/solidHelpers';
 import { useToast } from '#providers/ToastProvider';
@@ -8,11 +9,6 @@ import { useTRPC } from '#providers/TRPCProvider';
 import type { League } from '#shared/types/api/league';
 import { shortUUID } from '#shared/utils';
 import style from './AdminDashboard.module.scss';
-
-interface LeagueItemProps {
-  league: League;
-  activeLeague: League | null;
-}
 
 const getLeagues = query(async () => {
   const [{ client }] = useTRPC();
@@ -48,64 +44,97 @@ const getLeagueLink = async (uuid: string) => {
   return resp.link;
 };
 
-const LeagueItem: Component<LeagueItemProps> = (props) => {
-  const [, actions] = useToast();
-
-  const changeLeagueAction = useAction(changeLeague);
-
-  const handleGetLink = useHandleButtonAction(async () => {
-    const link = await getLeagueLink(props.league.uuid);
-
-    await navigator.clipboard.writeText(link);
-    actions.success(
-      `Magic link for ${props.league.alias} copied to the clipboard`,
-    );
-  });
-
-  const handleChangeLeague = useHandleButtonAction(async () => {
-    const result = await changeLeagueAction(props.league.uuid);
-
-    if (result.ok === false) {
-      actions.error(result.message ?? 'Failed to change league');
-    } else {
-      actions.success(`Switched to league: ${result.data.name}`);
-    }
-  });
-
-  return (
-    <div class={style.league}>
-      <span>{shortUUID(props.league.uuid)}</span>
-      <span class={style.name}> {props.league.name}</span>({props.league.alias})
-      <span
-        classList={{
-          [style.description!]: true,
-          [style.noDescription!]: !props.league.description,
-        }}
-      >
-        {' '}
-        {props.league.description}
-      </span>
-      <Button onPointerUp={handleGetLink} loading={handleGetLink.loading()}>
-        Get Link
-      </Button>
-      <Show when={props.activeLeague?.uuid !== props.league.uuid}>
-        <Button
-          onPointerUp={handleChangeLeague}
-          loading={handleChangeLeague.loading()}
-        >
-          Switch
-        </Button>
-      </Show>
-    </div>
-  );
-};
-
 export const AdminDashboard: Component = () => {
+  const [, actions] = useToast();
   const leagues = createAsync(() => getLeagues());
   const activeLeague = createAsync(() => getActiveLeague());
 
+  const changeLeagueAction = useAction(changeLeague);
+
+  const column: Column[] = [
+    {
+      key: 'uuid',
+      header: 'UUID',
+      align: 'left',
+      width: 8,
+      transform: (val) => shortUUID(val),
+    },
+    {
+      key: 'name',
+      header: 'Name',
+      align: 'left',
+    },
+    {
+      key: 'alias',
+      header: 'Alias',
+      align: 'left',
+    },
+    {
+      key: 'description',
+      header: 'Description',
+      align: 'left',
+    },
+    {
+      key: 'links',
+      header: 'Links',
+      align: 'center',
+      width: 13,
+      transform: (_, item) => {
+        const handleGetLink = useHandleButtonAction(async (league: League) => {
+          const link = await getLeagueLink(league.uuid);
+
+          await navigator.clipboard.writeText(link);
+          actions.success(
+            `Magic link for ${league.alias} copied to the clipboard`,
+          );
+        });
+
+        return (
+          <Button
+            severity="secondary"
+            onPointerUp={() => handleGetLink(item)}
+            loading={handleGetLink.loading()}
+          >
+            Get Link
+          </Button>
+        );
+      },
+    },
+    {
+      key: 'activate',
+      header: 'Activate',
+      align: 'center',
+      width: 10,
+      transform: (_, item) => {
+        const handleChangeLeague = useHandleButtonAction(
+          async (league: League) => {
+            const result = await changeLeagueAction(league.uuid);
+
+            if (result.ok === false) {
+              actions.error(result.message ?? 'Failed to change league');
+            } else {
+              actions.success(`Switched to league: ${result.data.name}`);
+            }
+          },
+        );
+
+        return (
+          <Show when={activeLeague()?.uuid !== item.uuid}>
+            <Button
+              severity="secondary"
+              onPointerUp={() => handleChangeLeague(item)}
+              loading={handleChangeLeague.loading()}
+            >
+              Switch
+            </Button>
+          </Show>
+        );
+      },
+    },
+  ];
+
   return (
-    <Widget topLeftLabels="Admin Dashboard">
+    <Widget class={style.container} topLeftLabels="Admin Dashboard">
       <div>
         Active League:{' '}
         {activeLeague()?.name ? (
@@ -117,11 +146,15 @@ export const AdminDashboard: Component = () => {
         )}
       </div>
       <Divider />
-      <For each={leagues()?.data}>
-        {(league) => (
-          <LeagueItem league={league} activeLeague={activeLeague() ?? null} />
-        )}
-      </For>
+
+      <div class={style.leaguesTableContainer}>
+        <Table
+          class={style.leaguesTable}
+          columns={column}
+          data={leagues()?.data ?? []}
+          classic={false}
+        />
+      </div>
     </Widget>
   );
 };
