1a5e5dc322c163fff1fe34bb0e413163afb054a0
Jakub Wiątek <92359584+jkbwtk@users.noreply.github.com>
2026-05-13 23:32:39 +0200

    Temporary rolldown native decorator workaround

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -1,14 +1,24 @@
+import babel from '@rolldown/plugin-babel';
 import { defineConfig } from 'rolldown';
+
+// https://main.vite.dev/guide/migration#javascript-transforms-by-oxc
+function decoratorPreset(options: Record<string, unknown>) {
+  return {
+    preset: () => ({
+      plugins: [['@babel/plugin-proposal-decorators', options]],
+    }),
+    rolldown: {
+      filter: {
+        code: '@',
+      },
+    },
+  };
+}
 
 export default defineConfig({
   platform: 'node',
 
-  transform: {
-    decorator: {
-      emitDecoratorMetadata: true,
-      legacy: true,
-    },
-  },
+  plugins: [babel({ presets: [decoratorPreset({ version: '2023-11' })] })],
 
   input: 'backend/server.ts',
   output: {
