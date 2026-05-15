e9421e71bf9329295352425eb5d436eb15d44331
Jakub Wiątek <92359584+jkbwtk@users.noreply.github.com>
2026-05-15 20:55:57 +0200

    Backport support for goal types

--- shared/types/Sheets.ts
+++ shared/types/Sheets.ts
@@ -28,11 +28,30 @@ const MatchEventBase = z.object({
   time: z.number().int(), // unix timestamp
 });
 
+export const GoalTypeEnum = {
+  AERIAL_GOAL: 'AERIAL_GOAL', // A goal scored by bouncing the ball off the rod or other non-figurine part of the table
+  BACK_STAB_GOAL: 'BACK_STAB_GOAL', // A goal scored by bouncing the ball off the back of the opponent's figurine
+  PARRY_GOAL: 'PARRY_GOAL', // A goal scored by parrying the ball kicked by the opponent into the goal
+  RETURN_TO_FIELD_GOAL: 'RETURN_TO_FIELD_GOAL', // A goal scored in a way that causes the ball to return to the field after crossing the goal line
+  TRICK_SHOT_GOAL: 'TRICK_SHOT_GOAL', // A goal scored by performing a trick shot, such as bouncing the ball off multiple figurines or the table
+  LONG_SHOT_GOAL: 'LONG_SHOT_GOAL', // A goal scored from a long distance
+  SLOW_GOAL: 'SLOW_GOAL', // A goal scored by slowly kicking the ball into the goal, catching the opponent off guard
+  FAST_GOAL: 'FAST_GOAL', // A goal scored by quickly kicking the ball into the goal, overwhelming the opponent
+  GUARD_PIERCE_GOAL: 'GUARD_PIERCE_GOAL', // A goal scored in a way that spun opponent's unheld or weakly held rod out of the way
+  PUSH_GOAL: 'PUSH_GOAL', // A goal scored by pushing the ball without making any audible sound or noticeable movement
+  BERMUDA_TRIANGLE_GOAL: 'BERMUDA_TRIANGLE_GOAL', // A goal scored directly after the ball hits one or both goalposts of the scoring side's goal
+} as const;
+
+export const GoalType = z.enum(GoalTypeEnum);
+
+export type GoalType = z.infer<typeof GoalType>;
+
 export const MatchEventGoal = MatchEventBase.extend({
   type: z.literal(MatchEventTypeEnum.GOAL),
   for: z.string(), // team color
   by: z.string(), //team color
   player: z.string().nullable().catch(null),
+  goalType: z.array(GoalType).catch([]),
 });
 
 export type MatchEventGoal = z.infer<typeof MatchEventGoal>;
