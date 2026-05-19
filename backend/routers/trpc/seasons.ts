8e1097a5ec40dae1a36d5017b0bacd1080a52d63
Jakub Wiątek <92359584+jkbwtk@users.noreply.github.com>
2026-05-19 23:37:05 +0200

    Wire seasons pagination on router level

--- backend/routers/trpc/seasons.ts
+++ backend/routers/trpc/seasons.ts
@@ -1,20 +1,32 @@
 import { TRPCError } from '@trpc/server';
 import z from 'zod';
 import { SeasonModel } from '#backend/db/models/SeasonModel';
+import { PaginationInput } from '#backend/types/trpc';
 import { runWithErrorConversion } from '#blib/modelErrors';
 import { leagueScopedProcedure, router } from '#blib/trpc';
 import { Season, SeasonCreate, SeasonUpdate } from '#shared/types/api/season';
 
 export const seasonsRouter = router({
-  seasons: leagueScopedProcedure.query(async ({ ctx }) => {
-    const seasons = SeasonModel.getAll(ctx.db, ctx.league.uuid);
-    const total = SeasonModel.count(ctx.db, ctx.league.uuid);
+  seasons: leagueScopedProcedure
+    .input(PaginationInput.optional())
+    .query(async ({ ctx, input }) => {
+      const seasons = runWithErrorConversion(() =>
+        SeasonModel.getAll(
+          ctx.db,
+          ctx.league.uuid,
+          input?.limit,
+          input?.offset,
+        ),
+      );
+      const total = runWithErrorConversion(() =>
+        SeasonModel.count(ctx.db, ctx.league.uuid),
+      );
 
-    return {
-      data: (await seasons).map((s) => s.serialize()),
-      total: await total,
-    };
-  }),
+      return {
+        data: (await seasons).map((s) => s.serialize()),
+        total: await total,
+      };
+    }),
 
   createSeason: leagueScopedProcedure
     .input(SeasonCreate)
@@ -49,10 +61,8 @@ export const seasonsRouter = router({
   seasonById: leagueScopedProcedure
     .input(Season.pick({ uuid: true }))
     .query(async ({ ctx, input }) => {
-      const season = await SeasonModel.getById(
-        ctx.db,
-        ctx.league.uuid,
-        input.uuid,
+      const season = await runWithErrorConversion(() =>
+        SeasonModel.getById(ctx.db, ctx.league.uuid, input.uuid),
       );
 
       if (!season) {
@@ -68,10 +78,8 @@ export const seasonsRouter = router({
   seasonByDate: leagueScopedProcedure
     .input(z.object({ date: z.date() }))
     .query(async ({ ctx, input }) => {
-      const season = await SeasonModel.getByDate(
-        ctx.db,
-        ctx.league.uuid,
-        input.date,
+      const season = await runWithErrorConversion(() =>
+        SeasonModel.getByDate(ctx.db, ctx.league.uuid, input.date),
       );
 
       return season?.serialize() ?? null;
