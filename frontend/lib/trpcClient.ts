5446ad54c1b318bc8aa338ac96a2efbb00a4a8f8
Jakub Wiątek <92359584+jkbwtk@users.noreply.github.com>
2026-05-20 18:46:49 +0200

    Consolidate retry links

--- frontend/lib/trpcClient.ts
+++ frontend/lib/trpcClient.ts
@@ -12,6 +12,33 @@ import type { AppRouter } from '#backend/routers/trpc/app';
 import { ssrLink } from '#flib/trpc';
 import { isDev } from '#flib/utils';
 
+const sharedRetryLink = retryLink({
+  retry: (opts) => {
+    const code = opts.error.data?.code;
+
+    if (opts.op.type === 'mutation') {
+      return false;
+    }
+
+    if (code && ['TIMEOUT', 'GATEWAY_TIMEOUT'].includes(code) === false) {
+      return false;
+    }
+
+    if (opts.attempts > 3) {
+      console.warn(`tRPC subscription max retries reached for ${opts.op.path}`);
+
+      return false;
+    }
+
+    if (isDev()) {
+      console.log(`tRPC subscription reconnecting (attempt ${opts.attempts})`);
+    }
+
+    return true;
+  },
+  retryDelayMs: (attempt) => Math.min(1000 * 2 ** attempt, 30000),
+});
+
 export const trpcClient = createTRPCClient<AppRouter>({
   links: [
     loggerLink({
@@ -23,56 +50,13 @@ export const trpcClient = createTRPCClient<AppRouter>({
       false: splitLink({
         condition: (op) => op.type === 'subscription',
         true: [
-          retryLink({
-            retry: (opts) => {
-              const code = opts.error.data?.code;
-
-              if (
-                code &&
-                ['TIMEOUT', 'GATEWAY_TIMEOUT'].includes(code) === false
-              ) {
-                return false;
-              }
-
-              if (opts.attempts > 120) {
-                console.warn(
-                  `tRPC subscription max retries reached for ${opts.op.path}`,
-                );
-                return false;
-              }
-
-              if (isDev()) {
-                console.log(
-                  `tRPC subscription reconnecting (attempt ${opts.attempts})`,
-                );
-              }
-
-              return true;
-            },
-            retryDelayMs: (attempt) => Math.min(1000 * 2 ** attempt, 30000),
-          }),
+          sharedRetryLink,
           httpSubscriptionLink({
             url: '/trpc',
           }),
         ],
         false: [
-          retryLink({
-            retry: (opts) => {
-              if (
-                opts.error.data &&
-                opts.error.data.code === 'INTERNAL_SERVER_ERROR'
-              ) {
-                return false;
-              }
-
-              if (opts.op.type === 'mutation') {
-                return false;
-              }
-
-              return opts.attempts <= 3;
-            },
-            retryDelayMs: (attempt) => Math.min(1000 * 2 ** attempt, 30000),
-          }),
+          sharedRetryLink,
           splitLink({
             condition: (op) => op.path.endsWith('$'),
             true: httpBatchLink({
