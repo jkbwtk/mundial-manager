f88e9d6ad4f9a500f9ba679ffbb2a415b4bcd662
Jakub Wiątek <92359584+jkbwtk@users.noreply.github.com>
2026-05-21 23:36:36 +0200

    Define basic dashboard grid layout

--- frontend/pages/DashboardLayout/DashboardLayout.tsx
+++ frontend/pages/DashboardLayout/DashboardLayout.tsx
@@ -1,5 +1,36 @@
+import { A } from '@solidjs/router';
+import { LogoSmall } from '#components/LogoSmall/LogoSmall';
+import { Divider } from '#components/Widget';
+import { isMobile } from '#flib/utils';
+import style from './DashboardLayout.module.scss';
+
 export const DashboardLayout: ParentComponent = (props) => {
-  return <>{props.children}</>;
+  console.log(isMobile());
+
+  return (
+    <div class={style.container}>
+      <div class={style.logo}>
+        <LogoSmall />
+      </div>
+
+      <Divider direction="vertical" connect={0b11} />
+
+      <div class={style.info} />
+
+      <Divider class={style.horizontalDivider} />
+
+      <div class={style.nav}>
+        <div class={style.inDevelopment}>
+          <div>Still in development</div>
+          Test pages -{'>'} <A href="/tests">link</A>
+        </div>
+      </div>
+
+      <Divider direction="vertical" connect={0b11} />
+
+      {props.children}
+    </div>
+  );
 };
 
 export default DashboardLayout;
