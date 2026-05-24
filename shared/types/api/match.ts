be79d0cdef79815ead393ed95a8aff5318c3ff8c
Jakub Wiątek <92359584+jkbwtk@users.noreply.github.com>
2026-05-24 20:52:42 +0200

    Update base Match api type

--- shared/types/api/match.ts
+++ shared/types/api/match.ts
@@ -1,4 +1,5 @@
 import z from 'zod';
+import { PaginatedResponse } from '#shared/zod';
 
 export const MatchStatusEnum = {
   SCHEDULED: 'SCHEDULED',
@@ -16,17 +17,24 @@ export type MatchStatus = z.infer<typeof MatchStatus>;
 export const Match = z.object({
   uuid: z.uuid(),
   tableUuid: z.uuid(),
+  ballUuid: z.uuid().nullish(),
 
   startDate: z.date(),
 
-  duration: z.number().int().nonnegative().nullable().default(null),
-  pauseDuration: z.number().int().nonnegative().default(0),
+  duration: z.number().int().nonnegative().nullish(),
+  pauseDuration: z.number().int().nonnegative().nullish(),
 
   status: MatchStatus,
 
-  hash: z.string(),
+  hash: z.string().readonly(),
 });
 export type Match = z.infer<typeof Match>;
+
+export const MatchPaginated = PaginatedResponse(Match);
+export type MatchPaginated = z.infer<typeof MatchPaginated>;
+
+export const MatchNullable = Match.nullable();
+export type MatchNullable = z.infer<typeof MatchNullable>;
 
 export const MatchCreate = Match.omit({ uuid: true });
 export type MatchCreate = z.infer<typeof MatchCreate>;
