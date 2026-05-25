41a4f2c21afcee7a9bb3218027883440caa35577
Jakub Wiątek <92359584+jkbwtk@users.noreply.github.com>
2026-05-25 23:58:35 +0200

    Fix MaterialSymbolTest vertical overflow

--- frontend/pages/MaterialSymbolTest/MaterialSymbolTest.tsx
+++ frontend/pages/MaterialSymbolTest/MaterialSymbolTest.tsx
@@ -1,26 +1,29 @@
 import { For } from 'solid-js';
 import { MaterialSymbol } from '#components/MaterialSymbol';
 import { Widget } from '#components/Widget';
-import { SupportedMaterialSymbol } from '#flib/supportedMaterialSymbols';
+import { SupportedMaterialSymbols } from '#flib/supportedMaterialSymbols';
+import style from './MaterialSymbolTest.module.scss';
 
 export const MaterialSymbolTest: Component = () => {
   return (
-    <Widget topLeftLabels="Material Symbol Test">
-      <For each={SupportedMaterialSymbol}>
-        {(symbol) => (
-          <div>
-            {symbol}
-            {' -> '}
-            <MaterialSymbol symbol={symbol} color="inherit" />
-            <MaterialSymbol symbol={symbol} color="primary" />
-            <MaterialSymbol symbol={symbol} color="gray" />
-            <MaterialSymbol symbol={symbol} color="blue" />
-            <MaterialSymbol symbol={symbol} color="green" />
-            <MaterialSymbol symbol={symbol} color="red" />
-            <MaterialSymbol symbol={symbol} color="yellow" />
-          </div>
-        )}
-      </For>
+    <Widget class={style.container} topLeftLabels="Material Symbol Test">
+      <div class={style.innerContainer}>
+        <For each={SupportedMaterialSymbols}>
+          {(symbol) => (
+            <div>
+              {symbol}
+              {' -> '}
+              <MaterialSymbol symbol={symbol} color="inherit" />
+              <MaterialSymbol symbol={symbol} color="primary" />
+              <MaterialSymbol symbol={symbol} color="gray" />
+              <MaterialSymbol symbol={symbol} color="blue" />
+              <MaterialSymbol symbol={symbol} color="green" />
+              <MaterialSymbol symbol={symbol} color="red" />
+              <MaterialSymbol symbol={symbol} color="yellow" />
+            </div>
+          )}
+        </For>
+      </div>
     </Widget>
   );
 };
