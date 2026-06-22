df2f86029e96b05afac3aebcd5f3fd27463bec2d
Jakub Wiątek <92359584+jkbwtk@users.noreply.github.com>
2026-06-22 20:57:59 +0200

    Update Required symbol

--- frontend/components/Required/Required.tsx
+++ frontend/components/Required/Required.tsx
@@ -1,5 +1,5 @@
 import style from './Required.module.scss';
 
 export const Required: Component = () => {
-  return <span class={style.required}>*</span>;
+  return <span class={style.required}>⁺</span>;
 };
