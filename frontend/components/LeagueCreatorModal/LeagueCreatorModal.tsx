069f23c4b09222e71d853af3bb9dbde425266919
Jakub Wiątek <92359584+jkbwtk@users.noreply.github.com>
2026-06-22 21:17:04 +0200

    Port LeagueCreatorModal to the new forms

--- frontend/components/LeagueCreatorModal/LeagueCreatorModal.tsx
+++ frontend/components/LeagueCreatorModal/LeagueCreatorModal.tsx
@@ -1,48 +1,22 @@
-import hljs from 'highlight.js/lib/core';
-import json from 'highlight.js/lib/languages/json';
-import { createMemo, createUniqueId, Show } from 'solid-js';
-import { Button } from '#components/Button';
-import { HighlightedCode } from '#components/HighlightedCode';
-import { Input } from '#components/Input';
-import { Modal } from '#components/Modal';
-import { Divider } from '#components/Widget';
-import { useFormValidation } from '#flib/formValidation';
-import { toJson } from '#flib/utils';
-import { useModalActions } from '#providers/ModalProvider';
-import { type League, LeagueCreate } from '#shared/types/api/league';
+import {
+  type League,
+  LeagueCreate,
+  LeagueUpdate,
+} from '#shared/types/api/league';
 import 'highlight.js/styles/gml.min.css';
-import { useAction } from '@solidjs/router';
-import { TextArea } from '#components/TextArea';
-import { autofocus } from '#flib/solidHelpers';
+import { ModalForm, type ModalFormLabels } from '#components/ModalForm';
 import { actionCreateLeague, actionUpdateLeague } from '#flib/trpcCalls';
-import { useToast } from '#providers/ToastProvider';
 import style from './LeagueCreatorModal.module.scss';
-
-hljs.registerLanguage('json', json);
-
 export interface LeagueCreatorModalProps {
   league?: League;
-}
-
-interface CreatorState {
-  title: string;
-  submitButtonText: string;
-  successMessage: string;
-  errorMessage: string;
-  logLabel: string;
 }
 
 export const LeagueCreatorModal: Component<LeagueCreatorModalProps> = (
   props,
 ) => {
-  const { closeModal } = useModalActions();
-  const [, actions] = useToast();
-
-  const formId = createUniqueId();
-
   const isCreating = props.league === undefined;
 
-  const config: CreatorState = isCreating
+  const config: ModalFormLabels = isCreating
     ? {
         title: 'League Creator',
         submitButtonText: 'Create',
@@ -58,131 +32,37 @@ export const LeagueCreatorModal: Component<LeagueCreatorModalProps> = (
         logLabel: 'League update error:',
       };
 
-  const { validate, errors, canSubmit, formSubmit } = useFormValidation(
-    LeagueCreate,
-    {
-      updateMode: !isCreating,
-    },
-  );
-
-  const hasValidationErrors = createMemo(() => Object.keys(errors).length > 0);
-
-  const handleError = (err: unknown) => {
-    actions.error(config.errorMessage);
-    console.error(config.logLabel, err);
-  };
-
-  const handleSuccess = (league: League) => {
-    actions.success(config.successMessage);
-    closeModal(league);
-  };
-
-  const handleCancel = (ev: PointerEvent) => {
-    ev.preventDefault();
-    closeModal(false);
-  };
-
-  const createAction = useAction(actionCreateLeague);
-  const updateAction = useAction(actionUpdateLeague);
-
-  if (!createAction || !updateAction) {
-    actions.error('Failed to initialize League Creator modal');
-    closeModal(false);
-    return;
-  }
-
-  const acton = (data: LeagueCreate) => {
-    if (isCreating) {
-      return createAction(data);
-    }
-
-    return updateAction({ ...data, uuid: props.league!.uuid });
-  };
-
-  const handleSubmit = formSubmit(acton, handleSuccess, handleError);
-
   return (
-    <Modal
+    <ModalForm
       class={style.modal}
-      topLeftLabels={config.title}
-      bottomLeftLabels={[
-        <Button severity="secondary" onPointerUp={handleCancel}>
-          Cancel
-        </Button>,
-      ]}
-      bottomRightLabels={[
-        <Button
-          type="submit"
-          form={formId}
-          loading={handleSubmit.isSubmitting()}
-          disabled={!canSubmit()}
-        >
-          {config.submitButtonText}
-        </Button>,
-      ]}
-    >
-      <form id={formId} class={style.form} onSubmit={handleSubmit}>
-        <div class={style.fieldRow}>
-          <span class={style.fieldLabel}>Name</span>
-          <Input
-            class={style.fieldInput}
-            type="text"
-            placeholder="League name"
-            minLength={3}
-            maxLength={64}
-            required
-            name="name"
-            value={props.league?.name ?? ''}
-            useDirectives={[validate, autofocus]}
-            invalid={!!errors.name}
-          />
-        </div>
-
-        <div class={style.fieldRow}>
-          <span class={style.fieldLabel}>Alias</span>
-          <Input
-            class={style.fieldInput}
-            type="text"
-            placeholder="Short id"
-            minLength={2}
-            maxLength={16}
-            required
-            name="alias"
-            value={props.league?.alias ?? ''}
-            useDirectives={[validate]}
-            invalid={!!errors.alias}
-          />
-        </div>
-
-        <div class={style.fieldRow}>
-          <span class={style.fieldLabel}>Description</span>
-          <TextArea
-            class={style.fieldInput}
-            placeholder="Optional"
-            maxLength={255}
-            name="description"
-            style={{ resize: 'vertical' }}
-            autocomplete="off"
-            value={props.league?.description ?? ''}
-            useDirectives={[validate]}
-            invalid={!!errors.description}
-          />
-        </div>
-
-        <Divider class={style.divider} />
-
-        <div class={style.hintRow}>
-          <Show
-            when={hasValidationErrors()}
-            fallback={<span class={style.noErrors}>No validation errors</span>}
-          >
-            <HighlightedCode
-              language="json"
-              code={`Errors: ${toJson(errors)}`}
-            />
-          </Show>
-        </div>
-      </form>
-    </Modal>
+      instance={props.league}
+      model={isCreating ? LeagueCreate : LeagueUpdate}
+      // @ts-expect-error
+      action={isCreating ? actionCreateLeague : actionUpdateLeague}
+      {...config}
+      fields={{
+        uuid: {
+          label: 'UUID',
+          type: 'text',
+          hidden: true,
+          implicitDefault: props.league?.uuid,
+        },
+        name: {
+          label: 'Name',
+          placeholder: 'League name...',
+          type: 'text',
+        },
+        alias: {
+          label: 'Alias',
+          placeholder: 'League alias...',
+          type: 'text',
+        },
+        description: {
+          label: 'Description',
+          placeholder: 'League description...',
+          type: 'textArea',
+        },
+      }}
+    />
   );
 };
