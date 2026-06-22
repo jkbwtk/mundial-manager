3301b3d1f42b22090447382d9e4ab924227d7438
Jakub Wiątek <92359584+jkbwtk@users.noreply.github.com>
2026-06-22 22:57:04 +0200

    Port TableCreator modal to the new forms

--- frontend/components/TableCreatorModal/TableCreatorModal.tsx
+++ frontend/components/TableCreatorModal/TableCreatorModal.tsx
@@ -1,46 +1,16 @@
-import { useAction } from '@solidjs/router';
-import hljs from 'highlight.js/lib/core';
-import json from 'highlight.js/lib/languages/json';
-import { createMemo, createUniqueId, Show } from 'solid-js';
-import { Button } from '#components/Button';
-import { HighlightedCode } from '#components/HighlightedCode';
-import { Input } from '#components/Input';
-import { Modal } from '#components/Modal';
-import { Divider } from '#components/Widget';
-import { useFormValidation } from '#flib/formValidation';
+import { ModalForm, type ModalFormLabels } from '#components/ModalForm';
 import { actionCreateTable, actionUpdateTable } from '#flib/trpcCalls';
-import { toJson } from '#flib/utils';
-import { useModalActions } from '#providers/ModalProvider';
-import { useToast } from '#providers/ToastProvider';
-import 'highlight.js/styles/gml.min.css';
-import { TextArea } from '#components/TextArea';
-import { autofocus } from '#flib/solidHelpers';
-import { type Table, TableCreate } from '#shared/types/api/table';
+import { type Table, TableCreate, TableUpdate } from '#shared/types/api/table';
 import style from './TableCreatorModal.module.scss';
-
-hljs.registerLanguage('json', json);
 
 export interface TableCreatorModalProps {
   table?: Table;
 }
 
-interface CreatorState {
-  title: string;
-  submitButtonText: string;
-  successMessage: string;
-  errorMessage: string;
-  logLabel: string;
-}
-
 export const TableCreatorModal: Component<TableCreatorModalProps> = (props) => {
-  const { closeModal } = useModalActions();
-  const [, actions] = useToast();
-
-  const formId = createUniqueId();
-
   const isCreating = props.table === undefined;
 
-  const config: CreatorState = isCreating
+  const config: ModalFormLabels = isCreating
     ? {
         title: 'Table Creator',
         submitButtonText: 'Create',
@@ -56,170 +26,56 @@ export const TableCreatorModal: Component<TableCreatorModalProps> = (props) => {
         logLabel: 'Table update error:',
       };
 
-  const { validate, errors, canSubmit, formSubmit } = useFormValidation(
-    TableCreate,
-    {
-      updateMode: !isCreating,
-      implicitDefaults: {
-        labels: [],
-      },
-    },
-  );
-
-  const hasValidationErrors = createMemo(() => Object.keys(errors).length > 0);
-
-  const handleError = (err: unknown) => {
-    actions.error(config.errorMessage);
-    console.error(config.logLabel, err);
-  };
-
-  const handleSuccess = (table: Table) => {
-    actions.success(config.successMessage);
-    closeModal(table);
-  };
-
-  const handleCancel = (ev: PointerEvent) => {
-    ev.preventDefault();
-    closeModal(false);
-  };
-
-  const createAction = useAction(actionCreateTable);
-  const updateAction = useAction(actionUpdateTable);
-
-  if (!createAction || !updateAction) {
-    actions.error('Failed to initialize Table Creator modal');
-    closeModal(false);
-    return;
-  }
-
-  const action = (data: TableCreate) => {
-    if (isCreating) {
-      return createAction(data);
-    }
-
-    return updateAction({
-      uuid: props.table!.uuid,
-      ...data,
-    });
-  };
-
-  const handleSubmit = formSubmit(action, handleSuccess, handleError);
-
   return (
-    <Modal
+    <ModalForm
       class={style.modal}
-      topLeftLabels={config.title}
-      bottomLeftLabels={[
-        <Button severity="secondary" onPointerUp={handleCancel}>
-          Cancel
-        </Button>,
-      ]}
-      bottomRightLabels={[
-        <Button
-          type="submit"
-          form={formId}
-          loading={handleSubmit.isSubmitting()}
-          disabled={!canSubmit()}
-        >
-          {config.submitButtonText}
-        </Button>,
-      ]}
-    >
-      <form id={formId} class={style.form} onSubmit={handleSubmit}>
-        <div class={style.fieldsContainer}>
-          <span class={style.fieldLabel}>Name:</span>
-          <Input
-            class={style.fieldInput}
-            type="text"
-            placeholder="Table name"
-            maxLength={255}
-            required
-            name="name"
-            value={props.table?.name ?? ''}
-            useDirectives={[validate, autofocus]}
-            invalid={!!errors.name}
-          />
-
-          <span class={style.fieldLabel}>Alias:</span>
-          <Input
-            class={style.fieldInput}
-            type="text"
-            placeholder="Table alias"
-            maxLength={255}
-            required
-            name="alias"
-            value={props.table?.alias ?? ''}
-            useDirectives={[validate]}
-            invalid={!!errors.alias}
-          />
-
-          <span class={style.fieldLabel}>Side #1 Color:</span>
-          <Input
-            class={style.fieldInput}
-            type="color"
-            placeholder="Side #1 Color"
-            maxLength={255}
-            required
-            name="side1Color"
-            value={props.table?.side1Color ?? ''}
-            useDirectives={[validate]}
-            invalid={!!errors.side1Color}
-          />
-
-          <span class={style.fieldLabel}>Side #2 Color:</span>
-          <Input
-            class={style.fieldInput}
-            type="color"
-            placeholder="Side #2 Color"
-            maxLength={255}
-            required
-            name="side2Color"
-            value={props.table?.side2Color ?? ''}
-            useDirectives={[validate]}
-            invalid={!!errors.side2Color}
-          />
-
-          <span class={style.fieldLabel}>Location:</span>
-          <Input
-            class={style.fieldInput}
-            type="text"
-            placeholder="Table location"
-            maxLength={255}
-            required
-            name="location"
-            value={props.table?.location ?? ''}
-            useDirectives={[validate]}
-            invalid={!!errors.location}
-          />
-
-          <span class={style.fieldLabel}>Description:</span>
-          <TextArea
-            class={style.fieldInput}
-            placeholder="Optional"
-            maxLength={255}
-            name="description"
-            style={{ resize: 'vertical' }}
-            autocomplete="off"
-            value={props.table?.description ?? ''}
-            useDirectives={[validate]}
-            invalid={!!errors.description}
-          />
-        </div>
-
-        <Divider class={style.divider} />
-
-        <div class={style.hintRow}>
-          <Show
-            when={hasValidationErrors()}
-            fallback={<span class={style.noErrors}>No validation errors</span>}
-          >
-            <HighlightedCode
-              language="json"
-              code={`Errors: ${toJson(errors)}`}
-            />
-          </Show>
-        </div>
-      </form>
-    </Modal>
+      instance={props.table}
+      model={isCreating ? TableCreate : TableUpdate}
+      // @ts-expect-error
+      action={isCreating ? actionCreateTable : actionUpdateTable}
+      {...config}
+      fields={{
+        uuid: {
+          label: 'UUID',
+          type: 'text',
+          hidden: true,
+          implicitDefault: props.table?.uuid,
+        },
+        name: {
+          label: 'Name',
+          placeholder: 'Table name...',
+          type: 'text',
+        },
+        alias: {
+          label: 'Alias',
+          placeholder: 'Table alias...',
+          type: 'text',
+        },
+        location: {
+          label: 'Location',
+          placeholder: 'Table location...',
+          type: 'text',
+        },
+        side1Color: {
+          label: 'Side 1 color',
+          type: 'color',
+        },
+        side2Color: {
+          label: 'Side 2 color',
+          type: 'color',
+        },
+        labels: {
+          label: 'Labels',
+          type: 'text',
+          hidden: true,
+          implicitDefault: props.table?.labels ?? [],
+        },
+        description: {
+          label: 'Description',
+          placeholder: 'Table description...',
+          type: 'textArea',
+        },
+      }}
+    />
   );
 };
