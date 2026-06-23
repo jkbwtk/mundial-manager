2ae129472b006d144a885b8a97c780943dce05c9
Jakub Wiątek <92359584+jkbwtk@users.noreply.github.com>
2026-06-23 23:28:52 +0200

    Update PlayersDashboard layout, add pagination

--- frontend/pages/PlayersDashboard/PlayersDashboard.tsx
+++ frontend/pages/PlayersDashboard/PlayersDashboard.tsx
@@ -1,22 +1,36 @@
 import { createAsync, useAction } from '@solidjs/router';
-import { getOwner, Show } from 'solid-js';
+import { createSignal, getOwner, Show } from 'solid-js';
 import { Button } from '#components/Button';
 import { ColorBlock } from '#components/ColorBlock';
+import { Paginator } from '#components/Paginator';
 import { PlayerCreatorModal } from '#components/PlayerCreatorModal/PlayerCreatorModal';
 import { type Column, Table } from '#components/Table';
-import { Divider, Widget } from '#components/Widget';
+import { Divider } from '#components/Widget';
 import { useHandleButtonAction } from '#flib/index';
 import { actionDeletePlayer, queryPlayers } from '#flib/trpcCalls';
 import { useModal } from '#providers/ModalProvider';
 import { useToast } from '#providers/ToastProvider';
-import type { Player } from '#shared/types/api/player';
+import type { Player, PlayerQueryMeta } from '#shared/types/api/player';
 import { shortUUID } from '#shared/utils';
 import style from './PlayersDashboard.module.scss';
 
 export const PlayersDashboard: Component = () => {
   const [, actions] = useToast();
   const [, { open }] = useModal();
-  const players = createAsync(() => queryPlayers());
+
+  const [limit, setLimit] = createSignal(50);
+  const [page, setPage] = createSignal(0);
+  const [sorting, setSorting] = createSignal<PlayerQueryMeta['sorting']>();
+
+  const queryMetaProp = (): PlayerQueryMeta => ({
+    pagination: {
+      limit: limit(),
+      offset: page() * limit(),
+    },
+    sorting: sorting(),
+  });
+
+  const players = createAsync(() => queryPlayers(queryMetaProp()));
 
   const deletePlayer = useAction(actionDeletePlayer);
   const owner = getOwner();
@@ -28,6 +42,13 @@ export const PlayersDashboard: Component = () => {
       },
       owner,
       closeOnBackgroundClick: false,
+    });
+  };
+
+  const handleOnSort = (field: string, direction: 'asc' | 'desc') => {
+    setSorting({
+      field: field as NonNullable<PlayerQueryMeta['sorting']>['field'],
+      direction,
     });
   };
 
@@ -43,16 +64,20 @@ export const PlayersDashboard: Component = () => {
       key: 'name',
       header: 'Name',
       align: 'left',
+      sortable: true,
     },
     {
       key: 'alias',
       header: 'Alias',
       align: 'center',
+      sortable: true,
     },
     {
       key: 'color',
       header: 'Color',
       align: 'center',
+      width: 12,
+      sortable: true,
       transform: (val) => <ColorBlock color={val} />,
     },
     {
@@ -111,22 +136,31 @@ export const PlayersDashboard: Component = () => {
   ];
 
   return (
-    <Widget class={style.container} topLeftLabels="Players Dashboard">
-      <div class={style.league}>
+    <div class={style.outerContainer}>
+      <div class={style.controls}>
         <Button onPointerUp={handleCreatePlayer}>Create Player</Button>
       </div>
 
+      <Paginator
+        total={players.latest?.total ?? 0}
+        limit={limit()}
+        setLimit={setLimit}
+        page={page()}
+        setPage={setPage}
+      />
+
       <Divider />
 
-      <div class={style.leaguesTableContainer}>
+      <div class={style.tableContainer}>
         <Table
-          class={style.leaguesTable}
+          class={style.table}
           columns={column}
-          data={players()?.data ?? []}
+          data={players.latest?.data ?? []}
           classic={false}
+          onSort={handleOnSort}
         />
       </div>
-    </Widget>
+    </div>
   );
 };
 
