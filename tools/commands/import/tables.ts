e2adf9502f5a77705fc2b416d545bae69fd0eb9e
Jakub Wiątek <92359584+jkbwtk@users.noreply.github.com>
2026-06-26 18:04:48 +0200

    Preserve more legacy data in imported tables

--- tools/commands/import/tables.ts
+++ tools/commands/import/tables.ts
@@ -6,6 +6,11 @@ import { logger } from '#shared/logger';
 import type { Match } from '#shared/types/Sheets';
 import type { ImportOptions } from '#tools/commands/import';
 
+interface LegacyFloor {
+  floor: number;
+  colors: Set<string>;
+}
+
 export async function importLegacyTables(
   options: ImportOptions,
   legacyMatches: Match[],
@@ -14,9 +19,20 @@ export async function importLegacyTables(
     label: ['cli', 'import', 'tables'],
   });
 
-  const legacyFloors = Array.from(
-    new Set(legacyMatches.map((match) => match.floor)),
-  ).filter((floor) => floor !== null);
+  const legacyFloors: Map<number, LegacyFloor> = new Map();
+
+  for (const match of legacyMatches) {
+    if (match.floor === null) continue;
+
+    const existing = legacyFloors.getOrInsert(match.floor, {
+      floor: match.floor,
+      colors: new Set(),
+    });
+
+    if (match.winningColor && match.winningColor !== 'unknown') {
+      existing.colors.add(match.winningColor);
+    }
+  }
 
   if (options.clear) {
     logger.info('Clearing existing data from tables...', {
@@ -30,21 +46,27 @@ export async function importLegacyTables(
     });
   }
 
-  logger.debug('Creating %d legacy tables...', legacyFloors.length, {
+  logger.debug('Creating %d legacy tables...', legacyFloors.size, {
     label: ['cli', 'import', 'tables'],
   });
 
   const tables = await Promise.all(
-    legacyFloors.map((floor) =>
-      TableModel.create(db, options.leagueUuid, {
-        name: `Floor ${floor}`,
-        alias: String(floor),
+    legacyFloors.values().map((floor) => {
+      const colors = Array.from(floor.colors).sort();
+
+      return TableModel.create(db, options.leagueUuid, {
+        name: `Floor ${floor.floor}`,
+        alias: String(floor.floor),
         side1Color: '#363636',
         side2Color: '#acacac',
-        location: `Floor ${floor}`,
-        labels: [],
-      }),
-    ),
+        location: `Floor ${floor.floor}`,
+        labels: [
+          `legacyImportFloor=${floor.floor}`,
+          `legacyImportSide1Color=${colors.at(0)}`,
+          `legacyImportSide2Color=${colors.at(1)}`,
+        ],
+      });
+    }),
   );
 
   logger.info('Successfully imported legacy tables', {
