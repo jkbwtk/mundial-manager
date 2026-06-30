8149d57ed392dbbc7ce214d974cd2f2f16d06156
Jakub Wiątek <92359584+jkbwtk@users.noreply.github.com>
2026-06-30 23:24:49 +0200

    Create getLabelValue helper method

--- shared/labels.ts
+++ shared/labels.ts
@@ -10,3 +10,25 @@ export type LabelValueTypes = z.infer<typeof LabelValueTypes>;
 
 export const Labels = z.record(z.string(), LabelValueTypes);
 export type Labels = z.infer<typeof Labels>;
+
+export function getLabelValue<
+  T extends z.ZodNull | z.ZodNumber | z.ZodString | z.ZodBoolean,
+>(
+  instance: { labels: Labels },
+  key: string,
+  type: T,
+  defaultValue?: z.infer<T>,
+): z.infer<T> {
+  const rawValue = instance.labels[key];
+
+  if (rawValue === undefined && defaultValue !== undefined) {
+    return defaultValue;
+  }
+
+  if (rawValue === undefined) {
+    throw new Error(`Label ${key} not found`);
+  }
+
+  // @ts-expect-error
+  return type.parse(rawValue);
+}
