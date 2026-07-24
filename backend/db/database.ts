20f2c628e0baab5e4e81df0bd790abac1dbd56e0
Jakub Wiątek <92359584+jkbwtk@users.noreply.github.com>
2026-07-24 19:31:47 +0200

    Use db connection pool

--- backend/db/database.ts
+++ backend/db/database.ts
@@ -1,25 +1,29 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
 import { relations } from '#backend/db/relations';
 import { environment } from '#backend/environment';
 import { lazyObject } from '#blib/lazyObject';
 import { logger } from '#shared/logger';
 
-export const db = lazyObject(() =>
-  drizzle({
-    connection: {
-      host: environment.POSTGRES_HOST,
-      port: environment.POSTGRES_PORT,
-      user: environment.POSTGRES_USER,
-      password: environment.POSTGRES_PASS,
-      database: environment.POSTGRES_DB,
+export const db = lazyObject(() => {
+  const client = new Pool({
+    host: environment.POSTGRES_HOST,
+    port: environment.POSTGRES_PORT,
+    user: environment.POSTGRES_USER,
+    password: environment.POSTGRES_PASS,
+    database: environment.POSTGRES_DB,
 
-      allowExitOnIdle: false,
-      keepAlive: true,
-    },
+    allowExitOnIdle: false,
+    keepAlive: true,
+    connectionTimeoutMillis: 500,
+  });
+
+  return drizzle({
+    client,
     relations,
     logger: environment.DATABASE_LOGGING ? logger : false,
-  }),
-);
+  });
+});
 
 export type DB = typeof db;
 
